def6c50b04e0e4e9610bdcfe17c4527a4140d561
Sai Surya Teja <saisuryateja3@gmail.com>
2024-12-17 22:06:53 +0530

    Added more column definition for review table

--- src/main/java/com/sst/UberReviewService/models/Review.java
+++ src/main/java/com/sst/UberReviewService/models/Review.java
@@ -1,12 +1,34 @@
 package com.sst.UberReviewService.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
 
     @Id // Primary key of the table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate // Tells spring that only handle it for object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // Tells spring about the format of Date Object to be stored i.e Date/ Time / TimeStamp
+    @LastModifiedDate // Tells spring that only handle it for object update
+    Date updatedAt;
 }
