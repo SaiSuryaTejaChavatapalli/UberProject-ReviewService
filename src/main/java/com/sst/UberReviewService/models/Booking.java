a2f68953eb6052b54ea9ba4f0dcedf6c64396b74
Sai Surya Teja <saisuryateja3@gmail.com>
2024-12-20 00:34:25 +0530

    @OneToOne mapping for review and Booking class

--- src/main/java/com/sst/UberReviewService/models/Booking.java
+++ src/main/java/com/sst/UberReviewService/models/Booking.java
@@ -1,19 +1,28 @@
 package com.sst.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
 @Entity
-public class Booking {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Booking extends BaseModel{
 
+    @OneToOne
+    private Review review; // We have a 1:1 relationship between booking and review
+    // Whenever we do composition , we have to do mapping
     @Enumerated(value = EnumType.STRING)
     private BookingStatus bookingStatus;
 
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date startTime;
 
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date endTime;
 
     private Long totalDistance;
