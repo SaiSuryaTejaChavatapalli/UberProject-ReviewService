1aa31c28f3bae3446abcf5fb1515327531eb5b75
Sai Surya Teja <saisuryateja3@gmail.com>
2024-12-23 13:35:26 +0530

    @JoinTable(     name='course_students',     joinColumns = @JoinColumn(name='student_id'),     inverseJoinColumns = @JoinColumn(name='course_id')     ) Many to Many Relationship Example

---
+++ src/main/java/com/sst/UberReviewService/practice/Student.java
@@ -0,0 +1,36 @@
+package com.sst.UberReviewService.practice;
+
+import com.sst.UberReviewService.models.BaseModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student extends BaseModel {
+
+    private String name;
+
+    private String rollNo;
+
+
+    @JoinTable(
+    name="course_students",
+    joinColumns = @JoinColumn(name="student_id"),
+    inverseJoinColumns = @JoinColumn(name="course_id")
+    )
+    @ManyToMany
+    private List<Course> courses=new ArrayList<>();
+
+}
