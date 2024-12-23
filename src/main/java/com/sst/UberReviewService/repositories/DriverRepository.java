6aed6e14613c574dab2bc3231051bc0a765da13e
Sai Surya Teja <saisuryateja3@gmail.com>
2024-12-23 19:23:51 +0530

    Hibernate query:@Query('SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :license')

--- src/main/java/com/sst/UberReviewService/repositories/DriverRepository.java
+++ src/main/java/com/sst/UberReviewService/repositories/DriverRepository.java
@@ -9,6 +9,12 @@ public interface DriverRepository extends JpaRepository<Driver,Long> {
     Optional<Driver> findByIdAndLicenseNumber(Long id, String licenseNumber);
 
 
+    // RAW Mysql query, error is thrown at runtime
     @Query(nativeQuery = true,value = "SELECT * FROM Driver WHERE id= :id AND license_number= :license")
     Optional<Driver> rawFindByIdAndLicenseNumber(Long id, String license);
+
+
+    //Hibernate query, error is thrown at compile time
+    @Query("SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :license")
+    Optional<Driver> hqlFindByIdAndLicense(Long id, String license);
 }
